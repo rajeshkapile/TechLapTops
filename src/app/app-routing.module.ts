58f75fa6f1bb69289028aaa7cfa465839ceb1c86
rajeshkapile <rajeshkapile97@gmail.com>
2023-01-03 19:46:41 +0530

    created New Project TechLaptops

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'aboutUs',component:AboutUsComponent},
+  {path:'home',component:HomeComponent},
+  {path:'service',component:ServicesComponent},
+  {path:'logIn',component:LoginComponent},
+  {path:'register',component:RegistrationComponent},
+
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDashBoardComponent } from './user-dash-board/user-dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AdminDashboardComponent,
+    UserDashBoardComponent
   ],
   imports: [
     BrowserModule,
